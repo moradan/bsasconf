9b65ca8b24679f2d31ed0af0332ab3bcc41fb354
moradan <realmoradan@hotmail.com>
2023-07-02 00:00:36 -0300

    re-estructurando la conexion SQL en clases

--- src/main/java/com/cac/bsasconf/ListarOradores.java
+++ src/main/java/com/cac/bsasconf/ListarOradores.java
@@ -1,87 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package com.cac.bsasconf;
 
 import com.cac.bsasconf.oradores.ModeloOradores;
-import com.cac.bsasconf.oradores.Orador;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.dbcp2.BasicDataSource;
 
-/**
- *
- * @author Moradan
- */
 @WebServlet(name = "ListarOradores", urlPatterns = {"/listarOradores"})
 public class ListarOradores extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ModeloOradores modelo = new ModeloOradores();
-        request.getRequestDispatcher("./WEB-INF/lista_oradores.jsp").forward(request, response);
-        
-        try {
-            ArrayList<Orador> listaOradores = modelo.getOradores();
-            System.out.println(listaOradores);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+        req.setAttribute("listaOradores", modelo.getOradores());
+        req.getRequestDispatcher("./WEB-INF/lista_oradores.jsp").forward(req, resp);
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
